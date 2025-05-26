1c8ca7b25a2eb3dcc387e71dc5fcb3d7ec9080ab
Mike Gehard <mike.gehard@gmail.com>
2025-05-26 06:47:26 -0400

    Type to drive transfofmation

--- tree-spotter/src/types.ts
+++ tree-spotter/src/types.ts
@@ -1,7 +1,43 @@
 export interface Env {
-    TREE_IMAGES: R2Bucket;
-    METADATA: KVNamespace;
-    IMAGE_PROCESSING_QUEUE?: Queue;
     TWILIO_AUTH_TOKEN: string;
     TWILIO_ACCOUNT_SID: string;
+}
+
+export interface TwilioMessage {
+    from: string;
+    images: TwilioImage[];
+}
+
+export interface TwilioImage {
+    url: string;
+    contentType: string;
+    contents: Promise<Buffer>
+}
+
+export interface TwilioImageLoadError {
+    message: string;
+}
+
+export interface GpsResolutionError {
+    message: string;
+}
+
+export interface DiameterCalculationError {
+    message: string;
+}
+
+export interface TreeImage {
+    contents: Buffer;
+    contentType: string;
+    gps: GpsCoordinate;
+    diameter: number;
+}
+
+export interface GpsCoordinate {
+    latitude: number;
+    longitude: number;
+}
+
+export interface Tree {
+    images: TreeImage[];
 }
